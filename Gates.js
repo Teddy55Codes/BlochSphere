3c6912d99111af5dade46e8afc9220ccbc00bff1
Teddy55Codes <teddy@teddy.email>
2024-07-14 19:44:40 +0200

    refactored Gates.js

--- Gates.js
+++ Gates.js
@@ -1,13 +1,13 @@
 import {Complex} from "./Complex";
 
 export const XGate = [[new Complex(0, 0), new Complex(1, 0)], 
-                      [new Complex(1, 0), new Complex(0, 0)]];
+                            [new Complex(1, 0), new Complex(0, 0)]];
 
 export const YGate = [[new Complex(0, 0), new Complex(0, -1)],
-                      [new Complex(0, 1), new Complex(0, 0)]];
+                            [new Complex(0, 1), new Complex(0, 0)]];
 
 export const ZGate = [[new Complex(1, 0), new Complex(0, 0)],
-                      [new Complex(0, 0), new Complex(-1, 0)]];
+                            [new Complex(0, 0), new Complex(-1, 0)]];
 
 export const HGate = [[new Complex(Math.SQRT1_2, 0), new Complex(Math.SQRT1_2, 0)],
-                      [new Complex(Math.SQRT1_2, 0), new Complex(-Math.SQRT1_2, 0)]];
+                            [new Complex(Math.SQRT1_2, 0), new Complex(-Math.SQRT1_2, 0)]];
